d34a1f07cf891f9eeac263e625403eea7ef7213b
Aye-Lwin-Soe <ayelwinsoe.it2018@gmail.com>
2021-01-11 22:32:26 +0630

    certificate migrate

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Certificate extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable=['verifycode','date','user_id','course_id'];
+
+    public function user(){
+      return $this->belongsTo('App\Models\User');
+    }
+
+    public function course(){
+      return $this->belongsTo('App\Models\Course');
+    }
+
 }
